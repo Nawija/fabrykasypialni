94645b2a02b287194c767474d3f3877685a49034
Valkon <96625039+Nawija@users.noreply.github.com>
2024-04-15 17:22:54 +0200

    Update Header.tsx

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -20,31 +20,32 @@ export default function Header() {
   }
 
   return (
-    <header
-      className={`${raleway.className} z-[999] w-full text-white transition-colors duration-300 ${
-        showMenu ? "bg-black/80 backdrop-blur-md" : ""
-      }`}
-    >
-      <div className="z-50 mx-auto flex max-w-[1600px] items-center justify-between px-7 py-3">
-        <Link href="/">
-          <Image src={Logo} alt="Fabryka Sypialni Logo" height={77} />
-        </Link>
-        <MenuBurger handleShowMenu={handleShowMenu} showMenu={showMenu} />
-      </div>
-
-      <nav className="bg-[#53236E] ">
-        <ul className="mb-6 flex w-full items-center justify-start max-w-screen-2xl mx-auto px-2 py-4 space-x-4">
+    <>
+      <header
+        className={`${raleway.className} z-[999] w-full text-white transition-colors duration-300 ${
+          showMenu ? "bg-black/80 backdrop-blur-md" : ""
+        }`}
+      >
+        <div className="z-50 mx-auto flex max-w-[1600px] items-center justify-between px-7 py-3">
+          <Link href="/">
+            <Image src={Logo} alt="Fabryka Sypialni Logo" height={77} />
+          </Link>
+          <MenuBurger handleShowMenu={handleShowMenu} showMenu={showMenu} />
+        </div>
+      </header>
+      <nav className="sticky top-0 bg-[#53236E] text-white z-[999]">
+        <ul className="mx-auto mb-6 flex w-full max-w-screen-2xl items-center justify-start space-x-6 px-2 py-4">
           {NAVLINKS.map((link) => (
             <li
               key={link.label}
               onClick={handleCloseMenu}
-              className={`cursor-pointer transition-all duration-300 hover:text-color ${showMenu ? "opacity-100" : "opacity-0 lg:opacity-100"}`}
+              className={`cursor-pointer capitalize transition-all duration-300 hover:text-gray-400 ${showMenu ? "opacity-100" : "opacity-0 lg:opacity-100"}`}
             >
               <Link href={link.href}>{link.label}</Link>
             </li>
           ))}
         </ul>
       </nav>
-    </header>
+    </>
   );
 }
