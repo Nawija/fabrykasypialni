c07da03d8e566dfdf8012eb9f98ec9d05040e11a
Valkon <96625039+Nawija@users.noreply.github.com>
2024-04-16 09:30:42 +0200

    gfdgdff

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -5,6 +5,7 @@ import Link from "next/link";
 import { useState } from "react";
 import MenuBurger from "./MenuBurger";
 
+import { FaSearch } from "react-icons/fa";
 import { FaPhoneVolume } from "react-icons/fa6";
 import { MdOutlineMailOutline } from "react-icons/md";
 import Logo from "../../../public/logo.png";
@@ -23,37 +24,40 @@ export default function Header() {
   return (
     <>
       <header
-        className={`z-[999] w-full text-black transition-colors duration-300 ${
+        className={`z-[999] w-full text-black transition-colors duration-300 overflow-x-hidden ${
           showMenu ? "bg-black/80 backdrop-blur-md" : ""
         }`}
       >
         <div className="z-50 mx-auto flex max-w-[1600px] items-center justify-between px-7 py-3">
           <div className="flex items-center justify-center space-x-4">
             <Link href="/">
-              <Image src={Logo} alt="Fabryka Sypialni Logo" height={140} />
+              <Image src={Logo} alt="Fabryka Sypialni Logo" height={100} />
             </Link>
-            <div className="flex flex-col items-start justify-center">
+            <div className="flex flex-col items-start justify-center text-lg">
               <Link
-                href="tel:555555555"
+                href="tel:223492851"
                 className="flex items-center justify-center text-color"
               >
-                <FaPhoneVolume /> <p className="pl-2">22 349 28 51</p>
+                <FaPhoneVolume className="text-xl" />{" "}
+                <p className="pl-2">22 349 28 51</p>
               </Link>
               <Link
                 href="mailto:sklep@fabrykasypialni.pl"
                 className="flex items-center justify-center"
               >
-                <MdOutlineMailOutline />
+                <MdOutlineMailOutline className="text-xl" />
                 <p className="pl-2">sklep@fabrykasypialni.pl</p>
               </Link>
             </div>
+            <div className="relative grow">
+              <input
+                placeholder="Szukaj produktu"
+                className="w-full border border-color/10 p-2 text-sm"
+              />
+              <FaSearch className="absolute right-0 top-1/2 -translate-x-1/2 -translate-y-1/2 text-color" />
+            </div>
           </div>
-          <div className="w-full">
-            <input
-              placeholder="Szukaj produktu"
-              className="w-3/4 mx-12 border border-color/10 p-2 text-sm"
-            />
-          </div>
+
           <div></div>
           <MenuBurger handleShowMenu={handleShowMenu} showMenu={showMenu} />
         </div>
@@ -66,7 +70,9 @@ export default function Header() {
               onClick={handleCloseMenu}
               className={`cursor-pointer capitalize transition-all duration-300 hover:text-stone-400 ${showMenu ? "opacity-100" : "opacity-0 lg:opacity-100"}`}
             >
-              <Link href={link.href}>{link.label}</Link>
+              <Link href={link.href} className="p-1">
+                {link.label}
+              </Link>
             </li>
           ))}
         </ul>
